56cef9cf73d1dd7685a2e9c41edc889825628603
SHAH NILAY JAYESH <58663029+njshah301@users.noreply.github.com>
2020-03-23 20:20:06 +0530

    Add files via upload

---
+++ encode3.m
@@ -0,0 +1,100 @@
+%Encoder
+close all;
+K=4;
+prompt="Please Enter The Value Of L";
+L=input(prompt);
+j=L;
+a=zeros(1,100000);
+m=randi([0,1],1,L);
+app=[0 0];
+m=cat(2,m,app);
+display(m);
+m=cat(2,m,a);
+m1=m(1);
+m2=0;
+m3=0;
+m4=0;
+i=1;
+x=[];
+
+while(L+2>=1)
+    
+    p1=mod(m1+m2+m3+m4,2);
+    p2=mod(m1+m2+m3,2);  
+    p3=mod(m1+m3+m4,2); 
+     
+x=cat(2,x,p1,p2,p3);
+m4=m3;
+m3=m2;
+m2=m1;
+m1=m(i+1);
+
+i=i+1;
+L=L-1;
+end
+
+display(x);
+L=j;
+
+%BSC Channel
+
+L=j;
+pError=0.5;
+Xbsc=x;
+nError=pError*(3*L+6);
+nError=floor(nError);
+count=0;
+nj=1;
+k=randperm((3*L)+6,nError);
+    k=sort(k);
+    s=zeros(1,10000);
+    k=cat(2,k,s);
+for i=1:((3*L)+6)
+    
+    if(i==k(nj))
+        if(Xbsc(i)==1)
+             Xbsc(i)='0';   
+        end
+        if(Xbsc(i)==0)
+            Xbsc(i)=1;
+        end
+        
+        nj=nj+1;
+    end
+end
+for i=1:((3*L)+6)
+    if(Xbsc(i)==48)
+        Xbsc(i)=mod(48,2);
+    end
+end
+display(Xbsc);
+            
+%BEC channel            
+    Xbec=x;
+   nj=1; 
+for i=1:((3*L)+6)
+    
+    if(i==k(nj))
+        if(Xbec(i)==1 || Xbec(i)==0)
+             Xbec(i)=8;   
+        end  
+        nj=nj+1;
+    end
+end
+
+display(Xbec);
+       %Gaussian Channel     
+            s=x;
+            for i=1:((3*L)+6)
+                s(i)=2*x(i)-1;
+            end
+            display(s);
+        SNRdB=1:((3*L)+6);    
+        SNRlin = 10.^(SNRdB/10);
+        r1=1/2;
+        sigma2=(1./(2*r1.*SNRlin));
+        sigma=sqrt(sigma2);
+        n=sigma*randn;
+        r=s+n;
+        display(r);
+            
